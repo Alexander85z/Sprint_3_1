3d32441b2d6d50ed635e24ef8d90e6a7c5ee0df2
Alexander <89161814170@mail.ru>
2022-11-03 17:49:19 +0300

    part 2

--- conftest.py
+++ conftest.py
@@ -1,36 +1,9 @@
 import pytest
 from selenium.webdriver.common.by import By
 from selenium import webdriver
-from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.support import expected_conditions
+
 from faker import Faker
 
-
-
-@pytest.fixture
-def driver():
-    driver = webdriver.Chrome()
-    driver.get("https://stellarburgers.nomoreparties.site/")
-
-    # открыть окно регистрации
-    driver.find_element(By.XPATH, ".//button[contains(text(),'Войти в аккаунт')]").click()
-    driver.find_element(By.XPATH, ".//a[contains(text(), 'Зарегистрироваться')]").click()
-
-    fake = Faker()
-    # ввести данные регистрации
-    driver.find_element(By.XPATH,
-                        ".//body/div[@id='root']/div[1]/main[1]/div[1]/form[1]/fieldset[1]/div[1]/div[1]/input[1]").send_keys(
-        'Дарт Вейдер')
-    driver.find_element(By.XPATH,
-                        ".//body/div[@id='root']/div[1]/main[1]/div[1]/form[1]/fieldset[2]/div[1]/div[1]/input[1]").send_keys(
-        'trewq123@mail.ru')
-    # можно создавать каждый раз уникальный емэил
-    #driver.find_element(By.XPATH,
-    #                    ".//body/div[@id='root']/div[1]/main[1]/div[1]/form[1]/fieldset[2]/div[1]/div[1]/input[1]").send_keys(
-    #    fake.email())
-
-    yield driver
-    driver.quit()
 
 @pytest.fixture
 def registration():
@@ -48,18 +21,15 @@ def registration():
         'Дарт Вейдер')
     registration.find_element(By.XPATH,
                         ".//body/div[@id='root']/div[1]/main[1]/div[1]/form[1]/fieldset[2]/div[1]/div[1]/input[1]").send_keys(
-        'trewq123@mail.ru')
+         fake.email())
     registration.find_element(By.XPATH,
                         ".//body/div[@id='root']/div[1]/main[1]/div[1]/form[1]/fieldset[3]/div[1]/div[1]/input[1]").send_keys(
         '123123')
     registration.find_element(By.XPATH, ".//button[contains(text(), 'Зарегистрироваться')]").click()
-    # можно создавать каждый раз уникальный емэил
-    #driver.find_element(By.XPATH,
-    #                    ".//body/div[@id='root']/div[1]/main[1]/div[1]/form[1]/fieldset[2]/div[1]/div[1]/input[1]").send_keys(
-    #    fake.email())
 
     yield registration
     registration.quit()
+
 
 @pytest.fixture
 def signin(driver):
